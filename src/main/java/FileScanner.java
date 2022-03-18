61461ea311f5789ac9595934edfe6d4a666e2a31
DenisAbashkin <35079479+DenisAbashkin@users.noreply.github.com>
2022-03-18 14:09:15 +0300

    add scanner file for IntLine file and convert it int array

---
+++ src/main/java/FileScanner.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FileScanner {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String path = "src\\main\\java\\IntLine";
+        File file = new File(path);
+
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        String[] numbersString = line.split(" ");
+        int[] numbers = new int[3];
+        int counter = 0;
+
+        for (String number : numbersString) {
+            numbers[counter++] = Integer.parseInt(number);
+        }
+
+
+        System.out.println(Arrays.toString(numbers));
+
+        scanner.close();
+    }
+}
